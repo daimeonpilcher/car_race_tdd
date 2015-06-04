27572233613cc41409691013f30a14fc46e0080c
DAIMEON PILCHER <daimeon.pilcher@gmail.com>
2015-06-03 18:52:16 -0700

    solved it

--- lib/race.rb
+++ lib/race.rb
@@ -1,5 +1,19 @@
 require_relative 'car'
 
 class Race
-  #write Race class code here
+  attr_accessor :cars # creates cars accessor
+  def initialize
+  	@cars = [] # creates @cars array
+  	car1 = Car.new(rand(0..100)) #Creates car with random speed at initialization
+  	car2 = Car.new(rand(0..100)) #Creates car with random speed at initialization
+  	@cars.push(car1, car2)
+  end
+  def winner
+  	return @cars[0] if @cars[0].speed > @cars[1].speed
+  	return @cars[1] if @cars[1].speed > @cars[0].speed
+  end
+  def loser
+  	return @cars[0] if @cars[0].speed < @cars[1].speed
+  	return @cars[1] if @cars[1].speed < @cars[0].speed
+  end
 end
